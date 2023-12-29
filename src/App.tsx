7c5d82a2669e12a5ca087238d8b28d649af73741
Hollin-A <ariyawansahollin@gmail.com>
2023-12-29 23:45:31 +0530

    feat: react router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="text-4xl">Hello World</div>;
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// importing layouts
+import RootLayout from "./layouts/RootLayout";
+
+// importing pages
+import Home from "./pages/Home";
+
+function App(): JSX.Element {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Outlet } from "react-router-dom";
+
+import NavBar from "../components/NavBar";
+
+const screenHeight = window.innerHeight;
+
+const bodyHeight = screenHeight - 48;
+
+type Props = {};
+
+const RootLayout = (props: Props) => {
+  return (
+    <div className="h-screen">
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default RootLayout;
