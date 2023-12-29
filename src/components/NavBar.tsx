c9077b3faef2dd98626908863d6579e2b78f97bb
Hollin-A <ariyawansahollin@gmail.com>
2023-12-29 23:53:20 +0530

    feat: basic navbar added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const NavBar = (props: Props) => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className="py-3">
+      <div className="container mx-auto flex items-center justify-end gap-5">
+        <p className="uppercase text-sm font-bold">admin</p>
+        <div className="bg-blue h-10 w-10 rounded-full" />
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
